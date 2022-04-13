ffcfb52f01c6ce3dbb4e0563c159f9a8e224a6aa
Nikolai Nachev <nikolaidan1@abv.bg>
2022-04-13 16:20:18 +0300

    Add files via upload

---
+++ 01. Day of Week/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] daysOfWeek = new string[7] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            if (n > 0 && n <= 7)
+            {
+                Console.WriteLine(daysOfWeek[n - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
