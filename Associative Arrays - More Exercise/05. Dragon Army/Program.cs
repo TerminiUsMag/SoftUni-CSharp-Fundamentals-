0a517ca09e7d620e0b4348cdddb361de08836186
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-04 17:35:49 +0300

    Starting a new exercise - 05.Dragon Army

---
+++ Associative Arrays - More Exercise/05. Dragon Army/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _05._Dragon_Army
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                //format "{type} {name} {damage} {health} {armor}".
+                //health 250, damage 45, and armor 10.Missing stat will be marked by null
+
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string type = command[0];
+                string name = command[1];
+                int damage;
+                int health;
+                int armor;
+                if (command[2] == "null")
+                {
+                    damage = 45;
+                }
+                else
+                {
+                    damage = int.Parse(command[2]);
+                }
+                if(command[3] == "null")
+                {
+                    health = 250;
+                }
+                else
+                {
+                    health = int.Parse(command[3]);
+                }
+                if(command[4] == "null")
+                {
+                    armor = 10;
+                }
+                else
+                {
+                    armor = int.Parse(command[4]);
+                }
+
+
+            }
+        }
+    }
+}
