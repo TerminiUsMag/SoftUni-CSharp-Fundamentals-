54851eeabd59065fb5c249b1c534b3df2b7bffd0
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-04 18:56:57 +0300

    Almost done but needs some more thinking :)

--- Associative Arrays - More Exercise/04. Snowwhite/Program.cs
+++ Associative Arrays - More Exercise/04. Snowwhite/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04._Snowwhite
 {
@@ -6,7 +8,52 @@ namespace _04._Snowwhite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string command;
+
+            var dwarfDict = new Dictionary<string, Dictionary<string, int>>();
+
+            while ((command = Console.ReadLine()) != "Once upon a time")
+            {
+                string[] commandTokens = command.Split("<:>", StringSplitOptions.RemoveEmptyEntries);
+
+                string dwarfName = commandTokens[0];
+                string dwarfHatColor = commandTokens[1];
+                int dwarfPhysics = int.Parse(commandTokens[2]);
+
+                if (dwarfDict.ContainsKey(dwarfHatColor))
+                {
+                    if (dwarfDict[dwarfHatColor].ContainsKey(dwarfName))
+                    {
+                        if (dwarfDict[dwarfHatColor][dwarfName] < dwarfPhysics)
+                        {
+                            dwarfDict[dwarfHatColor][dwarfName] = dwarfPhysics;
+                        }
+                    }
+                    else
+                    {
+                        dwarfDict[dwarfHatColor].Add(dwarfName, dwarfPhysics);
+                    }
+                }
+                else
+                {
+                    var newDict = new Dictionary<string, int>();
+                    newDict[dwarfName] = dwarfPhysics;
+                    dwarfDict.Add(dwarfHatColor, newDict);
+                }
+            }
+
+            var orderedDwarfDict = dwarfDict
+                .OrderByDescending(x=>x.Value.OrderByDescending(x=>x.Value))
+                .ThenByDescending(x=>x.Value.Count)
+                .ToDictionary(x=>x.Key,x=>x.Value);
+
+            foreach(var color in orderedDwarfDict)
+            {
+                foreach(var dwarf in color.Value)
+                {
+
+                }
+            }
         }
     }
 }
