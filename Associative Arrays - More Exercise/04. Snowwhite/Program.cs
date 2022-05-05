519863eaf3673a87567b3229f8c8519104db9ffb
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-05 18:47:00 +0300

    100/100 without classes :)

--- Associative Arrays - More Exercise/04. Snowwhite/Program.cs
+++ Associative Arrays - More Exercise/04. Snowwhite/Program.cs
@@ -58,77 +58,122 @@
 //    }
 //}
 
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace _04._Snowwhite
+//{
+//    class SnowWhite
+//    {
+//        static void Main(string[] args)
+//        {
+//            HashSet<Hat> hats = new HashSet<Hat>();
+//            List<Dwarf> dwarfs = new List<Dwarf>();
+//            string input = string.Empty;
+
+//            while ((input = Console.ReadLine()) != "Once upon a time")
+//            {
+//                string[] data = input.Split(new char[] { ' ', '<', ':', '>' }, StringSplitOptions.RemoveEmptyEntries);
+//                string name = data[0];
+//                string hat = data[1];
+//                long physics = long.Parse(data[2]);
+
+
+//                if (dwarfs.Select(x => x.Name).Contains(name) && dwarfs.Where(x => x.Name == name).Select(x => x.Hat.Name).Contains(hat))
+//                {
+//                    Dwarf repeatedDwarf = dwarfs.Single(x => x.Name == name && x.Hat.Name == hat);
+//                    repeatedDwarf.Physics = repeatedDwarf.Physics < physics ? physics : repeatedDwarf.Physics;
+//                }
+//                else
+//                {
+//                    if (hats.Select(x => x.Name).Contains(hat))
+//                    {
+//                        hats.Single(x => x.Name == hat).Count++;
+//                    }
+//                    else
+//                    {
+//                        hats.Add(new Hat(hat));
+//                    }
+//                    dwarfs.Add(new Dwarf(name, hats.Single(x => x.Name == hat), physics));
+//                }
+//            }
+
+//            foreach (Dwarf dwarf in dwarfs.OrderByDescending(x => x.Physics).ThenByDescending(x => x.Hat.Count))
+//            {
+//                Console.WriteLine($"({dwarf.Hat.Name}) {dwarf.Name} <-> {dwarf.Physics}");
+//            }
+//        }
+//    }
+
+//    class Dwarf
+//    {
+//        public Hat Hat { get; private set; }
+//        public string Name { get; private set; }
+//        public long Physics { get; set; }
+
+//        public Dwarf(string name, Hat hat, long physics)
+//        {
+//            this.Hat = hat;
+//            this.Name = name;
+//            this.Physics = physics;
+//        }
+//    }
+
+//    class Hat
+//    {
+//        public string Name { get; private set; }
+//        public long Count { get; set; }
+
+//        public Hat(string name)
+//        {
+//            this.Name = name;
+//            this.Count = 0;
+//        }
+//    }
+//}
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace _04._Snowwhite
 {
-    class SnowWhite
+    class Program
     {
         static void Main(string[] args)
         {
-            HashSet<Hat> hats = new HashSet<Hat>();
-            List<Dwarf> dwarfs = new List<Dwarf>();
-            string input = string.Empty;
-
-            while ((input = Console.ReadLine()) != "Once upon a time")
+            var dwarfs = new Dictionary<string, int>();
+            string input = Console.ReadLine();
+            while (input != "Once upon a time")
             {
-                string[] data = input.Split(new char[] { ' ', '<', ':', '>' }, StringSplitOptions.RemoveEmptyEntries);
-                string name = data[0];
-                string hat = data[1];
-                long physics = long.Parse(data[2]);
+                string[] inputs = input.Split(new string[] { " <:> " }, StringSplitOptions.RemoveEmptyEntries);
+                string name = inputs[0];
+                string color = inputs[1];
+                int physics = int.Parse(inputs[2]);
 
-
-                if (dwarfs.Select(x => x.Name).Contains(name) && dwarfs.Where(x => x.Name == name).Select(x => x.Hat.Name).Contains(hat))
+                string ID = name + ":" + color;
+                if (!dwarfs.ContainsKey(ID))
                 {
-                    Dwarf repeatedDwarf = dwarfs.Single(x => x.Name == name && x.Hat.Name == hat);
-                    repeatedDwarf.Physics = repeatedDwarf.Physics < physics ? physics : repeatedDwarf.Physics;
+                    dwarfs.Add(ID, physics);
                 }
                 else
                 {
-                    if (hats.Select(x => x.Name).Contains(hat))
-                    {
-                        hats.Single(x => x.Name == hat).Count++;
-                    }
-                    else
-                    {
-                        hats.Add(new Hat(hat));
-                    }
-                    dwarfs.Add(new Dwarf(name, hats.Single(x => x.Name == hat), physics));
+                    dwarfs[ID] = Math.Max(dwarfs[ID], physics);
                 }
-            }
 
-            foreach (Dwarf dwarf in dwarfs.OrderByDescending(x => x.Physics).ThenByDescending(x => x.Hat.Count))
+                input = Console.ReadLine();
+            }
+            foreach (var dwarf in dwarfs
+                .OrderByDescending(x => x.Value)
+                .ThenByDescending(x => dwarfs.Where(y => y.Key.Split(':')[1] == x.Key.Split(':')[1])
+                                             .Count()))
             {
-                Console.WriteLine($"({dwarf.Hat.Name}) {dwarf.Name} <-> {dwarf.Physics}");
+                Console.WriteLine("({0}) {1} <-> {2}",
+                    dwarf.Key.Split(':')[1],
+                    dwarf.Key.Split(':')[0],
+                    dwarf.Value);
             }
-        }
-    }
-
-    class Dwarf
-    {
-        public Hat Hat { get; private set; }
-        public string Name { get; private set; }
-        public long Physics { get; set; }
-
-        public Dwarf(string name, Hat hat, long physics)
-        {
-            this.Hat = hat;
-            this.Name = name;
-            this.Physics = physics;
-        }
-    }
-
-    class Hat
-    {
-        public string Name { get; private set; }
-        public long Count { get; set; }
-
-        public Hat(string name)
-        {
-            this.Name = name;
-            this.Count = 0;
         }
     }
 }
