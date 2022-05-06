b7c1904ef629b3ba83098a8136d84fe1ce63302b
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-06 19:32:40 +0300

    input and class added :)

--- Objects and Classes - More Exercise/05. Shopping Spree/Program.cs
+++ Objects and Classes - More Exercise/05. Shopping Spree/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Shopping_Spree
 {
+    class Employee
+    {
+        public Employee(string name, double salary, string department)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Department = department;
+        }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+        public string Name { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = inputs[0];
+                double salary = double.Parse(inputs[1]);
+                string department = inputs[2];
+
+                var newEmployee = new Employee(name, salary, department);
+
+                employees.Add(newEmployee);
+            }
         }
     }
 }
