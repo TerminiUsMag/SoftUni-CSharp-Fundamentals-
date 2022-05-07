55fdca428849c3b931937175f93beeb82449a7d8
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-07 12:51:42 +0300

    Added classes Person and Family to 02. :)

--- Objects and Classes - More Exercise/02. Oldest Family Member/Program.cs
+++ Objects and Classes - More Exercise/02. Oldest Family Member/Program.cs
@@ -1,7 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace _02._Oldest_Family_Member
 {
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Family
+    {
+        public List<Person> People { get; set; }
+        void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+        Person GetOldestMember()
+        {
+            int maxAge = 0;
+            Person maxMember = null;
+            foreach (var member in People)
+            {
+                if (member.Age > maxAge)
+                {
+                    maxAge = member.Age;
+                    maxMember = member;
+                }
+            }
+            return maxMember;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
