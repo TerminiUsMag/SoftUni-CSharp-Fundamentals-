4875501585725db2a7d014426223c6f4ebd74a19
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-07 15:12:40 +0300

    Needs more work :)

--- Objects and Classes - More Exercise/05. Shopping Spree/Program.cs
+++ Objects and Classes - More Exercise/05. Shopping Spree/Program.cs
@@ -25,6 +25,55 @@ namespace _05._Shopping_Spree
         public double Money { get; set; }
         public string Name { get; set; }
         public List<Product> Bag { get; set; }
+        public void AddProduct(Product product)
+        {
+            if (product.Cost > this.Money)
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+            else if (product.Cost <= this.Money)
+            {
+                this.Money -= product.Cost;
+                this.Bag.Add(product);
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+        }
+        public void ShoppingBag()
+        {
+            if (Bag.Count == 0)
+            {
+                Console.WriteLine($"{this.Name} - Nothing bought");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} - ");
+                if (this.Bag.Count > 1)
+                {
+                    //foreach (Product product in Bag)
+                    //{
+                    //    if (Bag.Last().Name == product.Name && Bag.Last().Cost == product.Cost)
+                    //    {
+                    //        Console.WriteLine($"{product.Name}");
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.Write($"{product.Name}, ");
+                    //    }
+                    //}
+                    for (int i = 0; i < this.Bag.Count; i++)
+                    {
+                        if (i == this.Bag.Count - 1)
+                        {
+                            Console.Write($"{this.Bag[i].Name}");
+                        }
+                        else
+                        {
+                            Console.Write($"{this.Bag[i].Name}, ");
+                        }
+                    }
+                }
+            }
+        }
     }
 
     internal class Program
@@ -56,6 +105,33 @@ namespace _05._Shopping_Spree
 
                 var newProduct = new Product(name, cost);
                 products.Add(newProduct);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = input[0];
+                string productName = input[1];
+
+                foreach (var person in people)
+                {
+                    if (person.Name == name)
+                    {
+                        foreach (var product2 in products)
+                        {
+                            if (product2.Name == productName)
+                            {
+                                person.AddProduct(product2);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var person in people)
+                {
+                    person.ShoppingBag();
+                }
             }
         }
     }
